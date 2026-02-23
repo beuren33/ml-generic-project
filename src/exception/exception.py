26e0e276b1546d9e14c379cdd089555bd24337a1
Matheus Beuren <matheusbeuren@gmail.com>
2026-02-23 07:11:19 -0300

    Logger and Exception Config

---
+++ src/exception/exception.py
@@ -0,0 +1,16 @@
+import sys
+
+def erro_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    erro_menssage = "Erro no arquivo: [{0}] na linha: [{1}], erro: [{2}]".format()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    file_name,exc_tb.tb_lineno,str(error)
+
+
+class Exception(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message,error_detail)
+        self.error_message = erro_message_detail(error_message,error_detail)
+    
+    def __str__(self):
+        return self.error_message
